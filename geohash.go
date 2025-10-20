d698edf13dc87b10a4d90890fbef4bf95f412c81
Abhishek Guha <abhi.workspace@gmail.com>
2025-10-20 23:14:38 +0200

    feat: Fix bugs and use bytes for usual 12 precision length

--- geohash.go
+++ geohash.go
@@ -17,14 +17,14 @@ const (
 // and bits from y in the odd. Checkout the following page many such bit tweaks
 // https://graphics.stanford.edu/~seander/bithacks.html#InterleaveBMN
 func interleave64(a, b uint32) uint64 {
-	B := []uint64{
+	B := [5]uint64{
 		0x5555555555555555,
 		0x3333333333333333,
 		0x0F0F0F0F0F0F0F0F,
 		0x00FF00FF00FF00FF,
 		0x0000FFFF0000FFFF,
 	}
-	S := []uint32{1, 2, 4, 8, 16}
+	S := [5]uint32{1, 2, 4, 8, 16}
 
 	x := uint64(a)
 	y := uint64(b)
@@ -47,6 +47,42 @@ func interleave64(a, b uint32) uint64 {
 	return x | (y << 1)
 }
 
+// De-Interleave 2 uint64's bit representation.
+func deinterleave64(i uint64) uint64 {
+	B := [6]uint64{
+		0x5555555555555555,
+		0x3333333333333333,
+		0x0F0F0F0F0F0F0F0F,
+		0x00FF00FF00FF00FF,
+		0x0000FFFF0000FFFF,
+		0x00000000FFFFFFFF,
+	}
+	S := [6]uint32{0, 1, 2, 4, 8, 16}
+
+	x := i
+	y := i >> 1
+
+	x = (x | (x >> S[0])) & B[0]
+	y = (y | (y >> S[0])) & B[0]
+
+	x = (x | (x >> S[1])) & B[1]
+	y = (y | (y >> S[1])) & B[1]
+
+	x = (x | (x >> S[2])) & B[2]
+	y = (y | (y >> S[2])) & B[2]
+
+	x = (x | (x >> S[3])) & B[3]
+	y = (y | (y >> S[3])) & B[3]
+
+	x = (x | (x >> S[4])) & B[4]
+	y = (y | (y >> S[4])) & B[4]
+
+	x = (x | (x >> S[5])) & B[5]
+	y = (y | (y >> S[5])) & B[5]
+
+	return x | (y << 32)
+}
+
 // Map each 5 bit to TEXTREP and produce a string representation of the geohash.
 func toChars(x uint64) string {
 	var s string
@@ -59,18 +95,35 @@ func toChars(x uint64) string {
 // Map each 5 bit to TEXTREP and produce a string representation of the geohash.
 func toCharsUnrolled(x uint64) string {
 	var s string
-	s += string(TEXTREP[(59&(((1<<5)-1)<<59))>>59])
-	s += string(TEXTREP[(54&(((1<<5)-1)<<54))>>54])
-	s += string(TEXTREP[(49&(((1<<5)-1)<<49))>>49])
-	s += string(TEXTREP[(44&(((1<<5)-1)<<44))>>44])
-	s += string(TEXTREP[(39&(((1<<5)-1)<<39))>>39])
-	s += string(TEXTREP[(34&(((1<<5)-1)<<34))>>34])
-	s += string(TEXTREP[(29&(((1<<5)-1)<<29))>>29])
-	s += string(TEXTREP[(24&(((1<<5)-1)<<24))>>24])
-	s += string(TEXTREP[(19&(((1<<5)-1)<<19))>>19])
-	s += string(TEXTREP[(15&(((1<<5)-1)<<15))>>15])
-	s += string(TEXTREP[(9&(((1<<5)-1)<<9))>>9])
-	s += string(TEXTREP[(4&(((1<<5)-1)<<4))>>4])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<59))>>59])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<54))>>54])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<49))>>49])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<44))>>44])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<39))>>39])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<34))>>34])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<29))>>29])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<24))>>24])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<19))>>19])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<14))>>14])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<9))>>9])
+	s += string(TEXTREP[(x&(((1<<5)-1)<<4))>>4])
+	return s
+}
+
+func toCharsUnrolledBytes(x uint64) [12]byte {
+	var s [12]byte
+	s[0] = TEXTREP[(x&(((1<<5)-1)<<59))>>59]
+	s[1] = TEXTREP[(x&(((1<<5)-1)<<54))>>54]
+	s[2] = TEXTREP[(x&(((1<<5)-1)<<49))>>49]
+	s[3] = TEXTREP[(x&(((1<<5)-1)<<44))>>44]
+	s[4] = TEXTREP[(x&(((1<<5)-1)<<39))>>39]
+	s[5] = TEXTREP[(x&(((1<<5)-1)<<34))>>34]
+	s[6] = TEXTREP[(x&(((1<<5)-1)<<29))>>29]
+	s[7] = TEXTREP[(x&(((1<<5)-1)<<24))>>24]
+	s[8] = TEXTREP[(x&(((1<<5)-1)<<19))>>19]
+	s[9] = TEXTREP[(x&(((1<<5)-1)<<14))>>14]
+	s[10] = TEXTREP[(x&(((1<<5)-1)<<9))>>9]
+	s[11] = TEXTREP[(x&(((1<<5)-1)<<4))>>4]
 	return s
 }
 
@@ -85,4 +138,8 @@ func GeoHashEncode(lat, lon float64) uint64 {
 	lonOffset *= 1 << STEP
 
 	return interleave64(uint32(latOffset), uint32(lonOffset))
+}
+
+func GeoHashDecode(s string) (float64, float64, error) {
+	return 0.0, 0.0, nil
 }
--- geohash_test.go
+++ geohash_test.go
@@ -10,10 +10,10 @@ func BenchmarkToChars(b *testing.B) {
 		14520001368503071193,
 	}
 
-	for _, v := range inputs {
-		n := fmt.Sprintf("%s_%d", b.Name(), v)
+	for i, v := range inputs {
+		n := fmt.Sprintf("%s_%d", b.Name(), i)
 		b.Run(n, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
+			for b.Loop() {
 				toChars(v)
 			}
 		})
@@ -25,11 +25,26 @@ func BenchmarkToCharsUnrolled(b *testing.B) {
 		14520001368503071193,
 	}
 
-	for _, v := range inputs {
-		n := fmt.Sprintf("%s_%d", b.Name(), v)
+	for i, v := range inputs {
+		n := fmt.Sprintf("%s_%d", b.Name(), i)
 		b.Run(n, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
+			for b.Loop() {
 				toCharsUnrolled(v)
+			}
+		})
+	}
+}
+
+func BenchmarkToCharsUnrolledBytes(b *testing.B) {
+	inputs := []uint64{
+		14520001368503071193,
+	}
+
+	for i, v := range inputs {
+		n := fmt.Sprintf("%s_%d", b.Name(), i)
+		b.Run(n, func(b *testing.B) {
+			for b.Loop() {
+				toCharsUnrolledBytes(v)
 			}
 		})
 	}
@@ -76,6 +91,50 @@ func TestToChars(t *testing.T) {
 			r := toChars(v.input)
 			if r != v.chars {
 				t.Errorf("%s: expected: %s got: %s", t.Name(), v.chars, r)
+			}
+		})
+	}
+}
+
+func TestToCharsUnrolledBytes(t *testing.T) {
+	testcases := []struct {
+		input uint64
+		chars string
+	}{
+		{
+			input: 14520001368503071193,
+			chars: "t60qhn3vtysx",
+		},
+	}
+
+	for _, v := range testcases {
+		n := fmt.Sprintf("%s_%d", t.Name(), v.input)
+		t.Run(n, func(t *testing.T) {
+			r := toCharsUnrolledBytes(v.input)
+			if string(r[:]) != v.chars {
+				t.Errorf("%s: expected: %s got: %+v", t.Name(), v.chars, r)
+			}
+		})
+	}
+}
+
+func TestToCharsUnrolled(t *testing.T) {
+	testcases := []struct {
+		input uint64
+		chars string
+	}{
+		{
+			input: 14520001368503071193,
+			chars: "t60qhn3vtysx",
+		},
+	}
+
+	for _, v := range testcases {
+		n := fmt.Sprintf("%s_%d", t.Name(), v.input)
+		t.Run(n, func(t *testing.T) {
+			r := toCharsUnrolled(v.input)
+			if r != v.chars {
+				t.Errorf("%s: expected: %s got: %+v", t.Name(), v.chars, r)
 			}
 		})
 	}
